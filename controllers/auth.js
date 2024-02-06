7269014fa60627e9fd741e578697de671f7d2744
Archer <shanebigornia123@gmail.com>
2024-02-06 10:04:10 +0800

    decrypt login fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-// const { promisify } = require("util");
 
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
@@ -10,81 +9,39 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-// exports.login = async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-//         if (!email || !password) {
-//             return res.status(400).sendFile(__dirname + "/login.hbs", {
-//                 message: "Please Provide an email and password"
-//             })
-//         }
-//         db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
-//             console.log(results);
-//             if (!results || !await bcrypt.compare(password, results[0].password)) {
-//                 res.status(401).sendFile(__dirname + '/login.hbs', {
-//                     message: 'Email or Password is incorrect'
-//                 })
-//             } else {
-//                 const id = results[0].id;
-
-//                 const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-//                     expiresIn: process.env.JWT_EXPIRES_IN
-//                 });
-
-//                 console.log("the token is " + token);
-
-//                 const cookieOptions = {
-//                     expires: new Date(
-//                         Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-//                     ),
-//                     httpOnly: true
-//                 }
-//                 res.cookie('userSave', token, cookieOptions);
-//                 res.status(200).redirect("/");
-//             }
-//         })
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
 exports.login = (req, res) => {
-
     console.log(req.body);
 
-    const { email, password } = req.body; //distructuring
+    const { email, password } = req.body;
 
-    db.query('SELECT * FROM blooddonor WHERE email = ? AND password = ?', [email] [password], async (error, results) => {
-
-        // var results = Object.values(JSON.parse(JSON.stringify(results)))
-
-        // if(error){
-        //     console.log(error);
-        // }
-    
-
-        // if(results.length > 0){
-        //     return res.render('login', {
-        //         message: 'Successfully logged in.'
-        //     })
-        // } else if(password !== passwordConfirm){
-        //     return res.render('login', {
-        //         message: 'Wrong password!.'
-        //     });
-        // }
-
-
-        if (!results || !await bcrypt.compare(password, results[0].password)) {
-                            return res.render('login', {
-                                message: 'Email or Password is incorrect'
-                            })
-        }else {
-            const id = results[0].id;
+    db.query('SELECT * FROM blooddonor WHERE email = ?', [email], async (error, results) => {
+        if (error) {
+            console.log(error);
+            return res.render('login', {
+                message: 'Something went wrong.'
+            });
         }
 
+        if (results.length === 0) {
+            return res.render('login', {
+                message: 'Email not found.'
+            });
+        }
+
+        const user = results[0];
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (isValidPassword) {
+            return res.render('login', {
+                message: 'Successfully logged in.'
+            });
+        } else {
+            return res.render('login', {
+                message: 'Wrong credentials.'
+            });
+        }
     });
-    
-}
+};
 
 exports.register = (req, res) => {
     console.log(req.body);
--- routes/pages.js
+++ routes/pages.js
@@ -10,7 +10,6 @@ router.get('/login', (req , res) => {
     res.render('login');
 });
 
-
 router.get('/register', (req , res) => {
     res.render('register');
 });
